766a268cb17a9f1a167c4139a1811e225cbadb0f
gogineni1998 <gogineni1998@gmail.com>
2022-02-13 14:01:47 +0530

    store project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from "react";
+import { MyContext } from "./context";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Stage_1 from "./components/stage_1";
+import Stage_2 from "./components/stage_2";
+import './App.css'
 
 function App() {
+  
+  const value = useContext(MyContext)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="center-wrapper">
+        <h1>Who Pays the Bill?</h1>
+        { value.obj.stage === 1 ? <Stage_1/> : <Stage_2/> }
+      </div>
     </div>
-  );
+  )   
 }
 
 export default App;
